370d5ce97c49c65c53fced90b763f2f429be8de3
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-03-17 17:27:49 -0300

    .

---
+++ public/resposta.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resposta</title>
+    <link rel="stylesheet" href="../ressources/css/style.css">
+</head>
+<body>
+    <main>
+        <div class="formula">
+            <?php
+            
+            
+            
+            
+            ?>
+
+        </div>
+    </main>
+    
+</body>
+</html>
