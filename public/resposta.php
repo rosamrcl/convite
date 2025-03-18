70d2136010d21de78fdc94d790c809dbc67f4715
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-03-18 14:49:59 -0300

    .

--- public/resposta.php
+++ public/resposta.php
@@ -7,17 +7,45 @@
     <link rel="stylesheet" href="../ressources/css/style.css">
 </head>
 <body>
-    <main>
-        <div class="formula">
-            <?php
-            
-            
-            
-            
-            ?>
+    <header>
+        <a href="./index.php"><i class="fa-solid fa-house" style="color: #e6ebf4;"></i></a>      
+        <h1>Festive</h1> 
+        <p>Crie um convite digital para seu evento</p>
+    </header>
+        <main>
+            <div class="formula">
+                <fieldset><legend>Dados do convite</legend>
 
-        </div>
-    </main>
+                <?php
+                if($_SERVER['REQUEST_METHOD']=="POST"){
+                    $evento=($_POST['evento']);
+                    $inicio=($_POST ['inicio']);
+                    $fim=($_POST['fim']);
+                    $tipoevento=($_POST['tipoevento']);
+                    $descricao=($_POST['descricao']);
+                    $temaevento=($_POST['temaevento']);
+                    $imagem=($_POST['imagem']);
+                    $nome-($_POST['nome']);
+                    $email=($_POST['email']);
+                    $telefone=($_POST['telefone']);
+                    $termos=($_POST['termos']);
+                    $aceitosms=($_POST['aceitosms']);
+
+
+                }
+                
+                
+                
+                ?>
+                </fieldset>
+
+            </div>
+        </main>
+    <footer>
+      <a href="https://github.com/RosaCL"
+        ><img src="../../public/img/costureza.png" alt=""
+      /></a>
+    </footer>
     
 </body>
 </html>
