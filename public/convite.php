cbb47e966978194921a122503be6b4d4c7c84bed
Rosa <costurezati@gmail.com>
2025-03-19 10:38:32 -0300

    .

--- public/convite.php
+++ public/convite.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resposta</title>
+    <title>Convite</title>
     <link rel="stylesheet" href="../ressources/css/style.css">
 </head>
 <body>
@@ -36,7 +36,6 @@
 
             echo "<p>
             <strong>Evento:</strong> $evento <br>
-
             <strong>Inicio:</strong> $inicio<br>
             <strong>fim:</strong> $fim<br>
             <strong>Tipo Evento:</strong> $tipoevento<br>
--- public/login.php
+++ public/login.php
@@ -3,41 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Resposta</title>
+    <link rel="stylesheet" href="../ressources/css/style.css">
 </head>
 <body>
     <header>        
-        <h1>Festive</h1>        
+        <h1>Festive</h1>
         <p>Crie um convite digital para seu evento</p>
-    </header> 
+    </header>
     <main>
-        
+        <div class="formula">
+            <fieldset><legend>Dados do convite</legend>
 
-            <div class="login-festa">
-                
-                    <fieldset> <legend>Festive</legend>
-                    <?php 
-                    if($_SERVER['REQUEST_METHOD']=='POST'){
-                        $login=$_POST['login'];
-                        $senha=$_POST['senha'];
-                    }elseif{
-                        header('Location:../ressources/views/login.html ');
-                    }else{
-                        header('Location:../ressources/views/convite.html');
-                    }
-                    
-                
-                    ?>
-                    
-                    </fieldset>
-        
+            <?php
+            if($_SERVER['REQUEST_METHOD']=="POST"){
+                $login=($_POST['login']);
+                $senha=($_POST ['senha']);
             
+                header("Location:../ressources/views/convite.html");
+            }
+    
+            ?>
+    
 
+            </fieldset>
+        </div>
     </main>
     <footer>
-        <a href="https://github.com/RosaCL"><img src="../../public/img/costureza.png" alt=""></a>
+        <a href="https://github.com/RosaCL"
+            ><img src="../../public/img/costureza.png" alt=""/></a>
     </footer>
 </body>
 </html>
